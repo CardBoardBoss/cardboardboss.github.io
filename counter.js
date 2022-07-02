11f17523ab510b1d8d704b263352697efabbe9ec
CardBoardBoss <somecoolsong@gmail.com>
2022-07-02 16:03:02 -0400

    Non Python Experiments

---
+++ counter.js
@@ -0,0 +1,27 @@
+if (!localStorage.getItem('counter')){
+  localStorage.setItem('counter', 0);
+}
+
+function count() {
+  let counter = localStorage.getItem('counter');
+  counter++;
+  document.querySelector('h1').innerHTML = counter;
+  localStorage.setItem('counter', counter);
+
+  if (counter % 10 === 0){
+    alert(`Count is now ${counter}`);
+  }
+}
+
+document.addEventListener('DOMContentLoaded', function(){
+  document.querySelector('h1').innerHTML = localStorage.getItem('counter');
+  document.querySelector('button').onclick = count;
+});
+
+document.addEventListener('DOMContentLoaded', function(){
+  document.querySelector('form').onsubmit = function(){
+    const name = document.querySelector('#name').value;
+    alert(`Hello, ${name}!`);
+
+  };
+});
